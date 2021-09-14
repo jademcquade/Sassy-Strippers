81118e80984928730708670109aa4a8915b50129
Jordan McQuade <jordanbmcquade@gmail.com>
2021-09-14 21:15:49 +0100

    added Contact section

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -2,79 +2,37 @@ import React from 'react';
 
 import { Section, Container } from '@components/global';
 
-import FaqItem from '@common/FaqItem';
-import ExternalLink from '@common/ExternalLink';
-
-const FAQS = [
-  {
-    title: 'How do I book a stripper?',
-    content: () => (
-      <>
-        Please use the contact form on our site to submit an enquiry. Make sure you include your preferred girl of choice on the form. We will respond to you as soon as possible.
-      </>
-    ),
-  },
-  {
-    title: 'Where will my stripper perform?',
-    content: () => (
-      <>
-        Gatsby’s rich data plugin ecosystem lets you build sites with the data
-        you want — from one or many sources: Pull data from headless CMSs, SaaS
-        services, APIs, databases, your file system, and more directly into your
-        pages using GraphQL.
-      </>
-    ),
-  },
-  {
-    title: 'What if I can\'t get my dancer of choice?',
-    content: () => (
-      <>
-        Gatsby.js is Internet Scale. Forget complicated deploys with databases
-        and servers and their expensive, time-consuming setup costs,
-        maintenance, and scaling fears. Gatsby.js builds your site as “static”
-        files which can be deployed easily on dozens of services.
-      </>
-    ),
-  },
-  {
-    title: 'What\'s expected of me?',
-    content: () => (
-      <>
-        Do not build a website with last decade’s tech. The future of the web is
-        mobile, JavaScript and APIs—the{` `}
-        <ExternalLink href="https://jamstack.org/">JAMstack</ExternalLink>.
-        Every website is a web app and every web app is a website. Gatsby.js is
-        the universal JavaScript framework you’ve been waiting for.
-      </>
-    ),
-  },
-  {
-    title: 'What exactly does Gatsby build?',
-    content: () => (
-      <>
-        Gatsby.js is a static PWA (Progressive Web App) generator. You get code
-        and data splitting out-of-the-box. Gatsby loads only the critical HTML,
-        CSS, data, and JavaScript so your site loads as fast as possible. Once
-        loaded, Gatsby prefetches resources for other pages so clicking around
-        the site feels incredibly fast.
-      </>
-    ),
-  },
-];
-
-const Faq = () => (
-  <Section id="faq">
+const Contact = () => (
+  <Section id="contact">
     <Container>
       <h1 style={{ marginBottom: 40 }}>Submit Enquiry</h1>
       <div>
-        {FAQS.map(({ title, content }) => (
-          <FaqItem title={title} key={title}>
-            {content()}
-          </FaqItem>
-        ))}
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>   
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
       </div>
     </Container>
   </Section>
 );
 
-export default Faq;
+export default Contact;
+
+
+
--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -4,26 +4,10 @@ import { StaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
 import { Container } from '@components/global';
-import ExternalLink from '@common/ExternalLink';
 
 import GithubIcon from '@static/icons/github.svg';
 import InstagramIcon from '@static/icons/instagram.svg';
 import TwitterIcon from '@static/icons/twitter.svg';
-
-const SOCIAL = [
-  {
-    icon: GithubIcon,
-    link: 'https://github.com/ajayns/gatsby-absurd',
-  },
-  {
-    icon: InstagramIcon,
-    link: 'https://instagram.com/ajay_ns',
-  },
-  {
-    icon: TwitterIcon,
-    link: 'https://twitter.com/ajayns08',
-  },
-];
 
 const Footer = () => (
   <StaticQuery
@@ -43,12 +27,6 @@ const Footer = () => (
     `}
     render={data => (
       <React.Fragment>
-        <Art>
-          <Img
-            fluid={data.art_pot.childImageSharp.fluid}
-            style={{ width: 480, maxWidth: '100%', marginBottom: -16 }}
-          />
-        </Art>
         <FooterWrapper>
           <StyledContainer>
             <Copyright>
@@ -64,20 +42,6 @@ const Footer = () => (
   />
 );
 
-const SocialIcons = styled.div`
-  display: flex;
-
-  img {
-    margin: 0 8px;
-    width: 24px;
-    height: 24px;
-  }
-
-  @media (max-width: ${props => props.theme.screen.sm}) {
-    margin-top: 40px;
-  }
-`;
-
 const FooterWrapper = styled.footer`
   background-color: ${props => props.theme.color.primary};
   padding: 32px 0;
@@ -92,13 +56,6 @@ const Copyright = styled.div`
     text-decoration: none;
     color: inherit;
   }
-`;
-
-const Art = styled.figure`
-  display: flex;
-  justify-content: center;
-  margin: 0;
-  margin-top: 48px;
 `;
 
 const StyledContainer = styled(Container)`
